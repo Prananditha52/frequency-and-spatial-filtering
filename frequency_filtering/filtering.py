9a041b6e04e854f51e7224584c0bb47d8961562a
Prananditha52 <90266599+Prananditha52@users.noreply.github.com>
2022-08-25 23:53:58 -0500

    Update filtering.py

--- frequency_filtering/filtering.py
+++ frequency_filtering/filtering.py
@@ -1,5 +1,4 @@
-# For this part of the assignment, You can use inbuilt functions to compute the fourier transform
-# You are welcome to use fft that are available in numpy and opencv
+
 
 import numpy as np
 
@@ -7,19 +6,12 @@ import numpy as np
 class Filtering:
 
     def __init__(self, image):
-        """initializes the variables frequency filtering on an input image
-        takes as input:
-        image: the input image
-        """
+
         self.image = image
         self.mask = self.get_mask
 
     def get_mask(self, shape):
-        """Computes a user-defined mask
-        takes as input:
-        shape: the shape of the mask to be generated
-        rtype: a 2d numpy array with size of shape
-        """
+
         mask = np.ones(shape)
         mask[225:245, 265:285] = 0
         mask[265:285,220:240]=0
@@ -30,17 +22,7 @@ class Filtering:
         return mask
 
     def post_process_image(self, image):
-        """Post processing to display DFTs and IDFTs
-        takes as input:
-        image: the image obtained from the inverse fourier transform
-        return an image with full contrast stretch
-        -----------------------------------------------------
-        You can perform post processing as needed. For example,
-        1. You can perfrom log compression
-        2. You can perfrom a full contrast stretch (fsimage)
-        3. You can take negative (255 - fsimage)
-        4. etc.
-        """
+
         a = 0
         b = 255
         min = np.min(image)
@@ -57,23 +39,7 @@ class Filtering:
 
 
     def filter(self):
-        """Performs frequency filtering on an input image
-        returns a filtered image, magnitude of DFT, magnitude of filtered DFT
-        ----------------------------------------------------------
-        You are allowed to used inbuilt functions to compute fft
-        There are packages available in numpy as well as in opencv
-        Steps:
-        1. Compute the fft of the image
-        2. shift the fft to center the low frequencies
-        3. get the mask (write your code in functions provided above) the functions can be called by self.filter(shape)
-        4. filter the image frequency based on the mask (Convolution theorem)
-        5. compute the inverse shift
-        6. compute the inverse fourier transform
-        7. compute the magnitude
-        8. You will need to do post processing on the magnitude and depending on the algorithm (use post_process_image to write this code)
-        Note: You do not have to do zero padding as discussed in class, the inbuilt functions takes care of that
-        filtered image, magnitude of DFT, magnitude of filtered DFT: Make sure all images being returned have grey scale full contrast stretch and dtype=uint8
-        """
+
         image = self.image
         initial_ft = np.fft.fft2(image)
         initial_shift_fft = np.fft.fftshift(initial_ft)
